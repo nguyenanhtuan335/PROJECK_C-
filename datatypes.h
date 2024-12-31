ce6cae60560116d47647d7ea35c929ef1e692a50
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-12-31 13:03:52 +0700

    Add files via upload

---
+++ datatypes.h
@@ -0,0 +1,16 @@
+#include<stdbool.h>
+   typedef struct {
+    char studentId[10];
+    char classroomId[10];
+    char name[20];
+    char birthDate[15];
+    char gender[10];
+    char email[30];
+    char phone[20];
+    char password[20];
+} Student;
+
+  
+  
+  
+  
